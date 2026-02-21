31499d1a5e452f32c11fd7f64cc73fa6ea938b72
traderpresley0-boop <traderpresley0@gmail.com>
2026-02-21 22:32:22 +0100

    Delete backend directory

--- backend/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import {createPayment} from "../controllers/paymentController.js"
-import auth from "../middleware/auth.js"
-
-const router=express.Router()
-
-router.post("/",auth,createPayment)
-
-export default router
