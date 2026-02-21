93f6ce2965556d88696adadd580a65c19e753724
traderpresley0-boop <traderpresley0@gmail.com>
2026-02-21 20:27:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,64 @@
-import {
-getStorage,
-ref,
-uploadBytes,
-getDownloadURL
-} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-storage.js";
-const storage = getStorage();
-async function enviarComprovante(){
-
-const file = document.getElementById("comprovante").files[0];
-
-if(!file){
-alert("Envie o comprovante primeiro");
-return;
-}
-
-const storageRef = ref(storage,"comprovantes/"+currentUser.uid);
-
-await uploadBytes(storageRef,file);
-
-const url = await getDownloadURL(storageRef);
-
-const userRef = doc(db,"usuarios",currentUser.uid);
-
-await updateDoc(userRef,{
-comprovante:url,
-status:"Aguardando confirmação",
-saldo:1000
-});
-
-alert("Comprovante enviado! Aguarde confirmação do administrador.");
-
-location.reload();
-  }
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+
+import {
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+
+import {
+getFirestore,
+doc,
+setDoc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKRygwlhw-re2luMd0LKfoDqu4Gsjz5Ts",
-  authDomain: "sorteio-iphone.firebaseapp.com",
-  projectId: "sorteio-iphone",
-  storageBucket: "sorteio-iphone.firebasestorage.app",
-  messagingSenderId: "744093528955",
-  appId: "1:744093528955:web:bed7451f5db2480b0c03de"
+apiKey: "SUA_API_KEY",
+authDomain: "SEU_PROJETO.firebaseapp.com",
+projectId: "SEU_PROJETO"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-// Criar conta
-window.registrar = function () {
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("senha").value;
 
-  createUserWithEmailAndPassword(auth, email, senha)
-    ..then(() => window.location.href = "dashboard.html")
-    .catch(error => alert(error.message));
+// CRIAR CONTA
+window.registrar = async function () {
+
+const email = document.getElementById("email").value;
+const senha = document.getElementById("senha").value;
+
+const userCredential = await createUserWithEmailAndPassword(auth, email, senha);
+
+await setDoc(doc(db, "usuarios", userCredential.user.uid), {
+email: email,
+saldo: 0,
+status: "novo",
+role: "user",
+createdAt: new Date()
+});
+
+alert("Conta criada!");
+window.location.href = "dashboard.html";
+};
+
+
+// LOGIN
+window.login = async function(){
+
+const email = document.getElementById("email").value;
+const senha = document.getElementById("senha").value;
+
+const userCredential = await signInWithEmailAndPassword(auth, email, senha);
+
+const userDoc = await getDoc(doc(db,"usuarios",userCredential.user.uid));
+
+if(userDoc.data().role === "admin"){
+window.location.href = "admin.html";
+}else{
+window.location.href = "dashboard.html";
+}
+
 };
